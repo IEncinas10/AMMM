7ffb0480c28c36b37e8f75560be0b4e29a1b7a76
IEncinas10 <ignacioencinasrubio@gmail.com>
2022-10-27 12:50:16 +0200

    Skeleton for .cpp impl

---
+++ greedy/problem.cpp
@@ -0,0 +1,175 @@
+#include <boost/algorithm/string.hpp>
+#include <cstdint>
+#include <fmt/core.h>
+#include <fmt/format.h>
+#include <fmt/ranges.h>
+#include <fstream>
+#include <iostream>
+#include <set>
+#include <sstream>
+
+using player_id = uint64_t;
+
+struct Player {
+    std::vector<uint64_t> points_per_day;
+
+    std::size_t &operator[](std::size_t index) { return points_per_day[index]; }
+    std::size_t operator[](std::size_t index) const { return points_per_day[index]; }
+};
+
+struct Match {
+    uint64_t day;
+    player_id white;
+    player_id black;
+
+    bool operator<(const Match &other) const {
+	return day < other.day;
+    }
+
+};
+
+
+struct Tournament {
+    std::size_t num_players;
+    // We can get the number of days from the
+    // number of players
+    // std::size_t num_days = num_players;
+
+    std::vector<Player> players;
+    std::multiset<Match> matches;
+
+    void set_num_players(std::size_t num_players_) {
+	num_players = num_players_;
+	players.resize(num_players);
+    }
+
+    void create_matchups() {
+	const uint64_t days = num_players; 
+	for(uint64_t day = 0; day < days; day++) {
+	    for(uint64_t white = 0; white < num_players; white++) {
+		for(uint64_t black = 0; black < num_players; black++) {
+		    const Match m { day, white, black }; 
+		    matches.insert(m);
+		}
+	    }
+	}
+    }
+
+    void print() {
+	fmt::print("{} players\n", num_players);
+	for (const auto &p : players) {
+	    fmt::print("[{}]\n", fmt::join(p.points_per_day, ", "));
+	}
+
+	create_matchups();
+	fmt::print("[\n\t{}\n]\n", fmt::join(matches, "\n\t"));
+    }
+};
+
+void get_num_players(const std::string &current_line, uint64_t &number_of_players,
+		     uint64_t &remaining_players) {
+    std::istringstream stream(current_line);
+    if (current_line.find('n') != std::string::npos) {
+	char dummy;
+	stream >> dummy >> dummy >> number_of_players;
+	remaining_players = number_of_players;
+    }
+}
+
+bool read_instance(const char *instance_filename, Tournament &tournament) {
+    std::ifstream input(instance_filename);
+    if (!input.is_open())
+	return false;
+
+    uint64_t lines_read = 0, players_read = 0, number_of_players = 0,
+	     remaining_players = UINT64_MAX, player_index = 0;
+    bool encountered_p = false;
+    std::string current_line;
+    do {
+	std::getline(input, current_line);
+	boost::algorithm::trim_if(current_line, boost::algorithm::is_any_of("[,], ,\t"));
+
+	// We haven't read players yet
+	if (number_of_players == 0) {
+	    get_num_players(current_line, number_of_players, remaining_players);
+	    tournament.set_num_players(number_of_players);
+	} else if (!encountered_p) {
+	    encountered_p = current_line.find('p') != std::string::npos;
+	} else if (!current_line.empty()) {
+	    std::istringstream stream(current_line);
+
+	    uint64_t number_of_days = number_of_players, tmp = 0;
+	    for (uint64_t j = 0; j < number_of_days; j++) {
+		stream >> tmp;
+		tournament.players[player_index].points_per_day.push_back(tmp);
+	    }
+	    player_index++;
+	    remaining_players--;
+	}
+
+	lines_read++;
+    } while (!input.eof() && remaining_players != 0);
+
+    fmt::print("Read {} lines\n", lines_read);
+
+    tournament.print();
+    return true;
+}
+
+void print_usage(const char *program_name) {
+    fmt::print("Usage: {} instance_filepath\n", program_name);
+}
+
+int main(int argc, char **argv) {
+    if (argc < 2) {
+	print_usage(argv[0]);
+    }
+
+    const char *instance_filename = argv[1];
+
+    Tournament tournament;
+    read_instance(instance_filename, tournament);
+}
+
+
+
+template <> struct fmt::formatter<Match> {
+  // Presentation format: 'f' - fixed, 'e' - exponential.
+  char presentation = 'f';
+
+  // Parses format specifications of the form ['f' | 'e'].
+  constexpr auto parse(format_parse_context& ctx) -> decltype(ctx.begin()) {
+    // [ctx.begin(), ctx.end()) is a character range that contains a part of
+    // the format string starting from the format specifications to be parsed,
+    // e.g. in
+    //
+    //   fmt::format("{:f} - point of interest", point{1, 2});
+    //
+    // the range will contain "f} - point of interest". The formatter should
+    // parse specifiers until '}' or the end of the range. In this example
+    // the formatter should parse the 'f' specifier and return an iterator
+    // pointing to '}'.
+
+    // Please also note that this character range may be empty, in case of
+    // the "{}" format string, so therefore you should check ctx.begin()
+    // for equality with ctx.end().
+
+    // Parse the presentation format and store it in the formatter:
+    auto it = ctx.begin(), end = ctx.end();
+    if (it != end && (*it == 'f' || *it == 'e')) presentation = *it++;
+
+    // Check if reached the end of the range:
+    if (it != end && *it != '}') throw format_error("invalid format");
+
+    // Return an iterator past the end of the parsed range:
+    return it;
+  }
+
+  // Formats the point p using the parsed format specification (presentation)
+  // stored in this formatter.
+  template <typename FormatContext>
+  auto format(const Match& m, FormatContext& ctx) const -> decltype(ctx.out()) {
+    // ctx.out() is an output iterator to write to.
+    return fmt::format_to(ctx.out(), "[Day: {}, White: {}, Black: {}]", m.day, m.white, m.black);
+  }
+};
