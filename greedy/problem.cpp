66951472dd2692993c355d9e680d4b284eddf09e
IEncinas10 <ignacioencinasrubio@gmail.com>
2022-11-08 08:34:10 +0100

    Fix error in evaluating can_play_black. Apply clang format

--- greedy/problem.cpp
+++ greedy/problem.cpp
@@ -23,12 +23,13 @@ struct Player {
     std::size_t &operator[](std::size_t index) { return points_per_day[index]; }
     std::size_t operator[](std::size_t index) const { return points_per_day[index]; }
 
-    static bool can_play_white(const Player &p) { return  p.games_white < (p.total_games() + 1)/2; }
-    static bool can_play_black(const Player &p) { return  p.games_black < (p.total_games() + 1)/2; }
+    static bool can_play_white(const Player &p) { return p.games_white < (p.total_games() + 1) / 2; }
+    static bool can_play_black(const Player &p) { return p.games_black < (p.total_games() + 1) / 2; }
 
-    bool operator==(const Player &other) const {
-	return playerID == other.playerID;
-    }
+    bool can_play_white() const { return games_white < (total_games() + 1) / 2; }
+    bool can_play_black() const { return games_black < (total_games() + 1) / 2; }
+
+    bool operator==(const Player &other) const { return playerID == other.playerID; }
 
     bool has_missing_games() const { return points_per_day.size() - 1 != games_black + games_white; }
     uint64_t total_games() const { return points_per_day.size() - 1; }
@@ -74,117 +75,119 @@ struct Tournament {
     }
 
     void create_matchups() {
-        // we assign the ID to the players
-        static const uint32_t MAX_TRIES = 100;
-        const uint64_t days = num_players;
-        for (uint64_t player = 0; player < num_players; player++) {
-            players[player].playerID = player;
-        }
+	// we assign the ID to the players
+	static const uint32_t MAX_TRIES = 100;
+	const uint64_t days = num_players;
+	for (uint64_t player = 0; player < num_players; player++) {
+	    players[player].playerID = player;
+	}
 
-        fmt::print("Creating all combination matches....\n");//we just create the possible matches we have, we will need to evaluate the points in each day.
+	fmt::print("Creating all combination matches....\n"); // we just create the possible matches we have, we will
+							      // need to evaluate the points in each day.
 
-        for (uint64_t white = 0; white < num_players; white++){
-            for (uint64_t black = 0; black < num_players; black++){
-                if(white == black)//we dont add matches vs yourself
-                    continue;
-                Game g {players[white].playerID, players[black].playerID};
-                games.push_back(g);                     //GAMES IS NOW THE CANDIDATE VECTOR
-            }
-        }
+	for (uint64_t white = 0; white < num_players; white++) {
+	    for (uint64_t black = 0; black < num_players; black++) {
+		if (white == black) // we dont add matches vs yourself
+		    continue;
+		Game g{players[white].playerID, players[black].playerID};
+		games.push_back(g); // GAMES IS NOW THE CANDIDATE VECTOR
+	    }
+	}
 
-        fmt::print("All matches possible matches created in C.\n");
+	fmt::print("All matches possible matches created in C.\n");
 
-        for(u_int64_t day = 0; day < days; day++){
-            uint64_t todayMatches = 0;
-            static const uint32_t MAX_TRIES = 10;
-            uint32_t tries = 0;
-            std::vector<uint64_t> todayPlayer;
-            fmt::print("DAY {}\nOrdering C by points\n", day);
+	for (u_int64_t day = 0; day < days; day++) {
+	    uint64_t todayMatches = 0;
+	    static const uint32_t MAX_TRIES = 10;
+	    uint32_t tries = 0;
+	    std::vector<uint64_t> todayPlayer;
+	    fmt::print("DAY {}\nOrdering C by points\n", day);
 
-            const std::vector<Player> players_day(players);
+	    const std::vector<Player> players_day(players);
 
-            std::sort(games.begin(), games.end(), [day, players_day](const Game &x, const Game &y){//sorting by less points per match
-                if((players_day[x.white].hasRested || players_day[x.black].hasRested)&&(!players_day[y.white].hasRested || !players_day[y.black].hasRested)){
-                    return true;
-                }
-                if((players_day[y.white].hasRested || players_day[y.black].hasRested)&&(!players_day[x.white].hasRested || !players_day[x.black].hasRested)){
-                    return false;
-                }
-                return players_day[(uint64_t) x.white].points_per_day[day] + players_day[(uint64_t) x.black].points_per_day[day] < players_day[(uint64_t) y.white].points_per_day[day] + players_day[(uint64_t) y.black].points_per_day[day];
-            });
+	    std::sort(games.begin(), games.end(),
+		      [day, players_day](const Game &x, const Game &y) { // sorting by less points per match
+			  if ((players_day[x.white].hasRested || players_day[x.black].hasRested) &&
+			      (!players_day[y.white].hasRested || !players_day[y.black].hasRested)) {
+			      return true;
+			  }
+			  if ((players_day[y.white].hasRested || players_day[y.black].hasRested) &&
+			      (!players_day[x.white].hasRested || !players_day[x.black].hasRested)) {
+			      return false;
+			  }
+			  return players_day[(uint64_t)x.white].points_per_day[day] +
+				     players_day[(uint64_t)x.black].points_per_day[day] <
+				 players_day[(uint64_t)y.white].points_per_day[day] +
+				     players_day[(uint64_t)y.black].points_per_day[day];
+		      });
 
-            fmt::print("\n");
-            for (Game &g : games){
-                fmt::print("{} - {}, ", g.white, g.black);
-            }
-            fmt::print("\n");
+	    fmt::print("\n");
+	    for (Game &g : games) {
+		fmt::print("{} - {}, ", g.white, g.black);
+	    }
+	    fmt::print("\n");
 
-            while(todayMatches < (num_players - 1) / 2){//restriccion de partidos por dia (?)
-                if(find_best_match(games, gamesPlayed, todayPlayer, day)){
-                    todayMatches++;
-                }else{
-                    fmt::print("no encontrado {}\n", tries);
-                    tries++;
-                }
-                if(tries > MAX_TRIES){
-                    break;
-                }
-            }
-            if(tries > MAX_TRIES){
-                fmt::print("FFFFFFFFFFFF\n");
-            }else fmt::print("DAY {} COMPLETED\n", day);
-                
-            
-        }
+	    while (todayMatches < (num_players - 1) / 2) { // restriccion de partidos por dia (?)
+		if (find_best_match(games, gamesPlayed, todayPlayer, day)) {
+		    todayMatches++;
+		} else {
+		    fmt::print("no encontrado {}\n", tries);
+		    tries++;
+		}
+		if (tries > MAX_TRIES) {
+		    break;
+		}
+	    }
+	    if (tries > MAX_TRIES) {
+		fmt::print("FFFFFFFFFFFF\n");
+	    } else
+		fmt::print("DAY {} COMPLETED\n", day);
+	}
     }
 
-    bool find_best_match(std::vector<Game> &games, std::vector<Game> &gamesPlayed, std::vector<uint64_t> &todayPlayer, uint64_t day){
-        //we search in array games (C) the feasibles matchups and the best suitable
-        for (Game &g : games){
-            if(games.size() == 0){
-                break;
-            }
-            uint64_t white = g.white;
-            uint64_t black = g.black;
-            //fmt::print("LOOKING FOR MATCH\n");
-            
-            //we check that both players didnt play today
-            auto pos = std::find(todayPlayer.begin(), todayPlayer.end(), white);
-            if (pos != todayPlayer.end()){
-                continue;
-            }
-            pos = std::find(todayPlayer.begin(), todayPlayer.end(), black);
-            if (pos != todayPlayer.end()){
-                continue;
-            }
+    bool find_best_match(std::vector<Game> &games, std::vector<Game> &gamesPlayed, std::vector<uint64_t> &todayPlayer,
+			 uint64_t day) {
+	// we search in array games (C) the feasibles matchups and the best suitable
+	for (Game &g : games) {
+	    if (games.size() == 0) {
+		break;
+	    }
+	    uint64_t white = g.white;
+	    uint64_t black = g.black;
+	    // fmt::print("LOOKING FOR MATCH\n");
 
-            //we check that they can play as black/white
-            if(!players[white].can_play_white){
-                continue;
-            }
-            if(!players[black].can_play_black){
-                continue;
-            }
-            const Match m {day, white, black};
-            fmt::print("Inserting match {} - {} day {}.\n", white, black, day);
-            matches.insert(m);
-            players[white].games_white++;
-            players[black].games_black++;
-            todayPlayer.push_back(white);
-            todayPlayer.push_back(black);
-            games.erase(std::find(games.begin(), games.end(), g));
-            Game gInverted {black, white};
-            games.erase(std::find(games.begin(), games.end(), gInverted));
-            fmt::print("Deleted from C.\n");
-            return true;
-            
-        }
-        return false;
+	    // we check that both players didnt play today
+	    auto pos = std::find(todayPlayer.begin(), todayPlayer.end(), white);
+	    if (pos != todayPlayer.end()) {
+		continue;
+	    }
+	    pos = std::find(todayPlayer.begin(), todayPlayer.end(), black);
+	    if (pos != todayPlayer.end()) {
+		continue;
+	    }
+
+	    // we check that they can play as black/white
+	    if (!players[white].can_play_white()) {
+		continue;
+	    }
+	    if (!players[black].can_play_black()) {
+		continue;
+	    }
+	    const Match m{day, white, black};
+	    fmt::print("Inserting match {} - {} day {}.\n", white, black, day);
+	    matches.insert(m);
+	    players[white].games_white++;
+	    players[black].games_black++;
+	    todayPlayer.push_back(white);
+	    todayPlayer.push_back(black);
+	    games.erase(std::find(games.begin(), games.end(), g));
+	    Game gInverted{black, white};
+	    games.erase(std::find(games.begin(), games.end(), gInverted));
+	    fmt::print("Deleted from C.\n");
+	    return true;
+	}
+	return false;
     }
-
-
-
-    
 
     void print() {
 	fmt::print("{} players\n", num_players);
@@ -196,8 +199,9 @@ struct Tournament {
 	fmt::print("[\n\t{}\n]\n", fmt::join(matches, "\n\t"));
 
 	for (const auto &p : players) {
-	    if(p.has_missing_games())
-		fmt::print("Player {} has played {} white games and {} black games\n", p.playerID, p.games_white, p.games_black);
+	    if (p.has_missing_games())
+		fmt::print("Player {} has played {} white games and {} black games\n", p.playerID, p.games_white,
+			   p.games_black);
 	}
     }
 };
